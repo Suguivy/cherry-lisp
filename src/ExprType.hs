4807eb0cbe44f53299dec7fd81a564e7c28703c6
Ivy <adrisolgo7373@gmail.com>
2021-01-30 22:07:16 +0100

    Changed display of expressions

--- src/ExprType.hs
+++ src/ExprType.hs
@@ -7,9 +7,16 @@ data Expr = IntE Integer
           | LambdaE String Expr
           | QuotedE Expr
           | NilE
-          deriving (Show)
 
--- instance Show Expr where
---   show (IntE x) = show x
---   show (VarE x) = x ++ " ; var"
---   show NilE     = "nil"
+-- Make set! and lambda(?) parsed as cons, detect later set! and lambda as special procedures
+
+instance Show Expr where
+  show (IntE x) = show x
+  show (VarE x) = x
+  show (SetE _ _) = "#set"
+  show c@(ConsE _ _) = "(" ++ showCons c
+    where showCons (ConsE _ NilE) = ")"
+          showCons (ConsE x xs)   = show x ++ " " ++ showCons xs
+  show (LambdaE s e) = "#lambda"
+  show (QuotedE e) = show e
+  show NilE     = "nil"
